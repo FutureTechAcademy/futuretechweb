05edc8e378123846e1506302acbe0a604c3aef9f
SWEETSIVA <rsuvisivadme2000@gmail.com>
2026-05-11 11:18:43 +0530

    j

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,8 @@ mongoose.connect(DB_PATH)
     })
 
 // Add Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors())
 
 // Add Static Files
