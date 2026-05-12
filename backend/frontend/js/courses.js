de40266c78568399df78b6933c7141da6c77e144
SWEETSIVA <rsuvisivadme2000@gmail.com>
2026-05-12 08:50:37 +0530

    g

--- backend/frontend/js/courses.js
+++ backend/frontend/js/courses.js
@@ -25,7 +25,7 @@ async function loadAllCourse() {
           <img src="${course.Image}" class="img-fluid">
         </div>
         <div class="card-content">
-          <h4 style="height:60px;" >${course.Title}</h4>
+          <h6 style="height:60px;" >${course.Title}</h6>
           <span>Duration: ${course.Duration}</span>
           <br>
           <a href="/course.html?id=${course._id}" class="btn btn-warning mt-2 w-100">View Details</a>
